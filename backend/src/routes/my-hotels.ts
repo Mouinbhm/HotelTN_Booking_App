4db75ca0607542678c160b3af109480368f26740
Ahmed Mouine Ben Hadj Mohamed <mouinbelhaj@icloud.com>
2025-03-22 16:47:37 +0100

    feat: add my-hotels route and hotel model for hotel management

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,68 @@
+import express, { Request, Response } from "express";
+import { v2 as cloudinary, v2 } from "cloudinary";
+import multer from "multer";
+import Hotel, { HotelType } from "../models/hotel";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+// api/my-hotels
+router.post(
+  "/",
+  verifyToken,
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("type").notEmpty().withMessage("Type is required"),
+    body("adultCount").notEmpty().withMessage("Adult count is required"),
+    body("childCount").notEmpty().withMessage("Child count is required"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities is required"),
+    body("pricePerNights")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required and must be a number"),
+    body("imageUrls")
+      .notEmpty()
+      .isArray()
+      .withMessage("Image URLs is required"),
+  ],
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
+
+      const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const res = await cloudinary.uploader.upload(dataURI);
+        return res.url;
+      });
+      const imageUrls = await Promise.all(uploadPromises);
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
+
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
+      res.status(201).send(hotel);
+    } catch (e) {
+      console.log("Error creating hotel", e);
+      res.status(500).send("Error creating hotel");
+    }
+  }
+);
+
+export default router;
