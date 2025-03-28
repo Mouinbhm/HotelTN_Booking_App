2ba8efe6ac360ed1df918e907bfdaedb1adb2c85
Ahmed Mouine Ben Hadj Mohamed <mouinbelhaj@icloud.com>
2025-03-28 14:54:07 +0100

    feat: update MongoDB connection string, enhance Express server routing, and improve authentication tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelsRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
+import path from "path";
 
 const UI_URL = "http://localhost:5174/";
 
-test("should allow the user to sign in", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto(UI_URL);
 
   // get the sign in button
@@ -16,33 +17,70 @@ test("should allow the user to sign in", async ({ page }) => {
   await page.getByRole("button", { name: "Login" }).click();
 
   await expect(page.getByText("Sign in Successful!")).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
 
-test("should allow user to register", async ({ page }) => {
-  const testEmail = `test_register_${
-    Math.floor(Math.random() * 90000) + 10000
-  }@test.com`;
-  await page.goto(UI_URL);
+test("should allow user to add a hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}add-hotel`);
 
-  await page.getByRole("link", { name: "Sign In" }).click();
-  await page.getByRole("link", { name: "Create an account here" }).click();
+  await page.locator('[name="name"]').fill("Test Hotel");
+  await page.locator('[name="city"]').fill("Test City");
+  await page.locator('[name="country"]').fill("Test Country");
+  await page
+    .locator('[name="description"]')
+    .fill("This is a description for the Test Hotel");
+  await page.locator('[name="pricePerNight"]').fill("100");
+  await page.selectOption('select[name="starRating"]', "3");
+
+  await page.getByText("Budget").click();
+
+  await page.getByLabel("Free Wifi").check();
+  await page.getByLabel("Parking").check();
+
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("4");
+
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "1.png"),
+    path.join(__dirname, "files", "2.png"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
+
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await expect(page.getByText("Dublin Getaways")).toBeVisible();
+  await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+  await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+  await expect(page.getByText("All Inclusive")).toBeVisible();
+  await expect(page.getByText("£119 per night")).toBeVisible();
+  await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+  await expect(page.getByText("2 Star Rating")).toBeVisible();
+
   await expect(
-    page.getByRole("heading", { name: "Create an Account" })
+    page.getByRole("link", { name: "View Details" }).first()
   ).toBeVisible();
+  await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});
 
-  await page.locator("[name=firstName]").fill("test_firstName");
-  await page.locator("[name=lastName]").fill("test_lastName");
-  await page.locator("[name=email]").fill(testEmail);
-  await page.locator("[name=password]").fill("password123");
-  await page.locator("[name=confirmPassword]").fill("password123");
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
 
-  await page.getByRole("button", { name: "Create Account" }).click();
+  await page.getByRole("link", { name: "View Details" }).first().click();
 
-  await expect(page.getByText("Registration Success!")).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
-  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Dublin Getaways UPDATED");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Dublin Getaways UPDATED"
+  );
+  await page.locator('[name="name"]').fill("Dublin Getaways");
+  await page.getByRole("button", { name: "Save" }).click();
 });
